1b487bd38054b861ef8bfca8ded45675d07285b1
Wender Emanuel <106766124+bahianoN30@users.noreply.github.com>
2024-06-27 23:11:00 -0300

    Update script.js

--- História/script.js
+++ História/script.js
@@ -6,70 +6,49 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Pedro estava em um relacionamento com Cleuza há quase dois anos. Eles se conheceram na faculdade e, desde então, compartilharam muitos momentos felizes. No entanto, ultimamente, Pedro sentia que algo estava faltando. Foi então que ele conheceu Roberta, uma nova colega de trabalho. Roberta era carismática, inteligente e tinha um sorriso que iluminava qualquer ambiente, qual das duas pedro ira escolher para ter um relacionamrneto no futuro.",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Cleuza",
+                afirmacao: "No início ficou com medo de dar tudo errado com Cleuza, mas acreditou no futuro de voces juntos."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Roberta",
+                afirmacao: "Você no fim da tudo certo com ela"
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Pedro decidiu ficar com Cleuza, acreditando que a estabilidade e a história que tinham juntos eram mais importantes do que a nova paixão por Roberta. Ele conversou com Cleuza sobre seus sentimentos confusos.",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Você decidiu dedicar-se totalmente ao relacionamento com Cleuza para melorar.",
+                afirmacao: "Cleuza ficou confusa de primeira, mas ainda assim confiou na sua palavra."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "voce tentou tirar tempo para voces dois, mas ainda assim você pensava na Roberta.",
+                afirmacao: "Cleuza viu que você tava estranho sobre esse relacionamnte."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Por um tempo, as coisas pareciam melhorar. Pedro e Cleuza começaram a passar mais tempo juntos e a se reconectar. No entanto, a sombra de Roberta nunca desapareceu completamente da mente de Pedro. Ele continuava pensando nela, e esses pensamentos gradualmente afetaram seu relacionamento com Cleuza, mas mesmo assim você acreditava que daria certo com Cleuza ainda e não desistiu.",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Voce decidiu que Cleuza tava se tornando a mulher perfeita para sua vida e você esqueceu de Roberta.",
+                afirmacao: "Voces estão melhores que nunca e estão se entendendo super bem"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Você decidiu que quer largar Cleuza pois você e ela não estão mais bem juntos, e quer focar Só na roberta",
                 afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
+        enunciado: "Eventualmente, a tensão acumulada chegou ao ponto de ruptura. Cleuza, cansada de lutar por um amor que parecia perdido, decidiu terminar o relacionamento. Pedro se jogou no colo de Roberta e no fim deu tudo certo, pois como ele esperava ela era a mulher certa para ele.",
+ 
     },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
-        ]
-    },
+    
 ];
 
 
